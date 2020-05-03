f87645a629cb974fb958cd27fb6c65b688770fa2
Torsten <torsten.manhardt@gmx.de>
2020-05-03 20:38:49 +0200

    path corrections

--- test/test.js
+++ test/test.js
@@ -1,21 +1,21 @@
 var express = require('express');
 var nedb = require('nedb');
-var expressNedbRest = require('../index.js');
+var path = require('path');
+var expressNedbRest = require(path.join(__dirname, '../index.js'));
 
 // setup express app
 var oApp = express();
 
-
 // local files for test frontend
-oApp.use('/index.html', express.static('index.html'));
-oApp.use('/spin.gif', express.static('spin.gif'));
+oApp.use('/index.html', express.static(path.join(__dirname, 'index.html')));
+oApp.use('/spin.gif', express.static(path.join(__dirname, 'spin.gif')));
 
 
 // create REST service and NEDB datastores
 var restApi = expressNedbRest();
-restApi.addDatastore('fruits',  new nedb({ filename: "fruits.db",  autoload: true }));
-restApi.addDatastore('animals', new nedb({ filename: "animals.db", autoload: true }));
-restApi.addDatastore('test',    new nedb({ filename: "test.db", autoload: true }));
+restApi.addDatastore('fruits',  new nedb({ filename: path.join(__dirname, 'fruits.db'),  autoload: true }));
+restApi.addDatastore('animals', new nedb({ filename: path.join(__dirname, 'animals.db'), autoload: true }));
+restApi.addDatastore('test',    new nedb({ filename: path.join(__dirname, 'test.db'), autoload: true }));
 
 // connect /rest path to REST service
 oApp.use('/rest', restApi);
